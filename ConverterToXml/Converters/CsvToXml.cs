77e2524dac644047b08d56fbaf380170b577f934
Pieliedie <Haketx@gmail.com>
2022-07-29 12:17:55 +0300

    refactor usings

--- ConverterToXml/Converters/CsvToXml.cs
+++ ConverterToXml/Converters/CsvToXml.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Xml.Linq;
-using DocumentFormat.OpenXml.Math;
 using Microsoft.VisualBasic.FileIO;
 
 namespace ConverterToXml.Converters
