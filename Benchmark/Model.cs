abb7dfe6a029d4327ceda5852afa873d346931f5
ПономаревОВ <pov@infostandart.com>
2024-01-09 10:18:03 +0300

    cleanup

--- Benchmark/Model.cs
+++ Benchmark/Model.cs
@@ -1,18 +1,11 @@
 ﻿using BenchmarkDotNet.Attributes;
-using ConverterToXml.Test;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 using ConverterToXml.Core.Converters;
 
 namespace Benchmark
 {
     public class Model
     {
-        public XlsxToXmlTest xlsx = new XlsxToXmlTest();
-
         [Benchmark]
         public void smallxlsx()
         {
